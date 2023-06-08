086dea3438e09f0100de678c04d60cdd2d97fd98
TrisqiTegarValenzi <trisqitegarv@gmail.com>
2023-06-08 08:55:17 +0700

    akun

--- resources/views/transfer/add_transfer.blade.php
+++ resources/views/transfer/add_transfer.blade.php
@@ -293,26 +293,7 @@
                                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                 </div>
                                                 <div class="modal-body">
-                                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                                    <div class="field-wrapper">
-                                                    <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-12">
-                                                        <div class="text-radio">Jenis<span class="text-danger">*</span></div>
-                                                    </div>
-                                                        <div class="half-width-container">
-
-                                                            <input type="radio" name="option" id="option1" value="Option 1" checked>
-                                                            <label class="label" for="option1">Bank</label>
-
-                                                            <input type="radio" name="option" id="option2" value="Option 2">
-                                                            <label class="label" for="option2">Kartu Kredit</label>
-                                                        </div>
-                                                    </div>
-
-                                                    <!-- Field wrapper start -->
-
-                                                    <!-- Field wrapper end -->
-
-                                                    </div>
+                                                
 
                                                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 
