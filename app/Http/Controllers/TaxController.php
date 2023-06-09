d8eb391db87a786ebf826ee56513bed6a24fe1a5
TrisqiTegarValenzi <trisqitegarv@gmail.com>
2023-06-09 09:50:44 +0700

    tambah kolom type pada tabel tax

--- app/Http/Controllers/TaxController.php
+++ app/Http/Controllers/TaxController.php
@@ -2,21 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\pajak;
+use App\Models\PajakModel;
 use Illuminate\Http\Request;
 
 class TaxController extends Controller
 {
     public function tax_index()
     {
-        return  view('Tax.index');
+        $data = pajak::all();
+        return  view('Tax.index', compact('data'));
     }
+
     public function tax_add()
     {
         return  view('Tax.add');
     }
 
-    public function tax_edit()
+    public function tax_insert(Request $request)
     {
-        return  view('Tax.edit');
+        pajak::create($request->all());
+        return redirect()->route('tax');
+    }
+
+    public function tax_edit($id)
+    {
+        $data = pajak::find($id);
+        return  view('Tax.edit', compact('data'));
+    }
+
+    public function tax_update(Request $request, $id)
+    {
+        $data = pajak::find($id);
+        $data->update($request->all());
+        return redirect()->route('tax');
+    }
+
+    public function tax_delete($id)
+    {
+        $data = pajak::find($id);
+        $data->delete();
+        return redirect()->route('tax');
     }
 }
